4be00f0cba900e6cc401daf4f2dff6bea7fb1ff1
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2017-02-02 10:35:38 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeUniqueID/PrismDeUniqueID/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeUniqueID.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
